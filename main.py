d0d81a3633592f86520bd0853df52c163fbabca7
Alexcsharp17 <55028956+Alexcsharp17@users.noreply.github.com>
2020-06-15 19:16:28 +0300

    Added site parser

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from bs4 import BeautifulSoup
 def web_hook_triger(first, second, third):
     report = {}
     report["value1"] = first
@@ -7,5 +7,19 @@ def web_hook_triger(first, second, third):
     report["value3"] = third
     requests.post("https://maker.ifttt.com/trigger/data_entered/with/key/bauu9B0Ig2Rgp4fpi7qTc4", data=report)
 
-while(True):
-    web_hook_triger("a", "b", "c")
+
+web_hook_triger("a", "b", "c")
+
+class SitePars:
+    searString= 'https://www.anekdot.ru/release/anekdot/day/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125'}
+
+    def GetData(self):
+        full_page = requests.get(self.SearString, headers=self.headers)
+        soup = BeautifulSoup(full_page.content, 'html.parser')
+        anecdotes=soup.findAll('div',{'class':'text'})[0].text
+        web_hook_triger(anecdotes,'test','test')
+def Main():
+   s= SitePars()
+   s.GetData()
